39ab09e33ec5fdccb2df08849003cacb2f82b9ef
Nick <koteloks@yandex.ua>
2015-11-10 23:32:43 +0200

    MoneyApp

---
+++ src/com/frame/app/ShowMoneyFrame.java
@@ -0,0 +1,8 @@
+package com.frame.app;
+
+class ShowMoneyFrame {
+    
+    public static void main(String args[]) {
+        new MoneyFrame();
+    }
+}
--- src/com/frame/app/SimpleFrameActions.java
+++
@@ -1,7 +0,0 @@
-package com.frame.app;
-
-/**
- * Created by admin on 11/10/15.
- */
-public class SimpleFrameActions {
-}
